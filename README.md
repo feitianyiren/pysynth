f9e234e3b0f4500fda2d9502b1383474c0a15df3
Will Noble <wnoble@mit.edu>
2014-12-09 21:42:38 -0500

    fixed some readability issues

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ pysynth
 =======
 
 A simple yet versatile music synthesis tool with a python front-end (and back-end, currently)
+
+see song.py for more information
+
--- song.py
+++ song.py
@@ -13,12 +13,12 @@ s0 = Sample.from_function(math.sin, note_freq('C'), 6000.0, domain=range(10000))
 s1 = Sample.from_function(math.sin, note_freq('E'), 6000.0, domain=range(10000))  # middle D
 s2 = Sample.from_function(math.sin, note_freq('G'), 6000.0, domain=range(10000))  # middle G
 
-dev.loop(s0, 1)  # plays C every beat
-dev.loop(s1, 2)  # plays E every 2 beats
-dev.loop(s2, 4)  # plays G every 4 beats
 # Default time signature is 60 bpm
+dev.loop(s0, 1)  # plays C every beat (ever second at 60 bpm)
+dev.loop(s1, 2)  # plays E every 2 beats (once ever 2 seconds at 60 bpm)
+dev.loop(s2, 4)  # plays G every 4 beats (once ever 4 seconds at 60 bpm)
 
-dev.start()  # open an audio device and start playing
+dev.start()  # open an audio device and start playing. Playback is handled by a separate thread.
 sleep(5.0)   # let the loops run for 5 seconds
-dev.close()  # close the audio device
+dev.close()  # close the audio device (and join the playback thread)
 
